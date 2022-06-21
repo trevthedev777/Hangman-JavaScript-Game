7bc8525566d9e5de06583c3984102845a32e6c5f
Your Name <you@example.com>
2022-06-21 14:00:45 -0400

    Create play again function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,90 +1,116 @@
-// DOM Elements
-const wordElement = document.getElementById("word");
-const wrongLettersElement = document.getElementById("wrong-letters");
-const playAgainBtn = document.getElementById("play-button");
-const popup = document.getElementById("popup-container");
-const notification = document.getElementById("notification-container");
-const finalMessage = document.getElementById("final-message");
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
 
-// The Hangman Body Elements
-const figureParts = document.querySelector("figure-part");
-// Connect an API to generate random words in future releases
-const words = [
-    "application", 
-    "programming", 
-    "interface", 
-    "wizard"
-];
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-// Set right and wrong choices as empty arrays
 const correctLetters = [];
 const wrongLetters = [];
 
-// Show the hidden word
+// Show hidden word
 function displayWord() {
-    wordElement.innerHTML = `
-        ${selectedWord
-        .split("")
-        .map(
-            letter => `
-                <span class="letter">
-                    ${correctLetters.includes(letter) ? letter : ""}
-                </span>
-            `)
-        .join("")}
-    `;
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(
+        letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+      )
+      .join('')}
+  `;
 
-    // When the user wins the round
-    const innerWord = wordElement.innerHTML.replace(/\n/g, "");
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
 
-    if (innerWord === selectedWord) {
-        finalMessage.innerText = "Congratulations!, You won! 😀";
-        popup.style.display = "flex";
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations! You won! 😃';
+    popup.style.display = 'flex';
+  }
+}
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
     }
-};
+  });
 
-// Update wrong letters element
-function updateWrongLettersElement() {
-    console.log("Update Wrong");
-};
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost. 😕';
+    popup.style.display = 'flex';
+  }
+}
 
-// show notification
+// Show notification
 function showNotification() {
-    notification.classList.add("show");
+  notification.classList.add('show');
 
-    setTimeout(() => {
-        notification.classList.remove("show");
-    }, 2000);
-};
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
 
-// Keydown Letter press
-// setting the window as the event listener for key press and keydown
-window.addEventListener("keydown", e => {
-    // console.log(e.keycode)
-    if (e.code >= 'KeyA' && e.code <= 'KeyZ') {
-        const letter = e.key;
+// Keydown letter press
+window.addEventListener('keydown', e => {
+  // console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
 
-        if (selectedWord.includes(letter)) {
-            if (!correctLetters.includes(letter)) {
-                correctLetters.push(letter);
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
 
-                displayWord();
-            } else {
-                showNotification();
-            }
-        } else {
-            if (!wrongLetters.includes(letter)) {
-                wrongLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
 
-                updateWrongLettersElement();
-            } else {
-                showNotification();
-            }
-        }
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
     }
+  }
 });
 
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  //  Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
+});
 
 displayWord();
